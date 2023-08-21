d2b3a06575c7ad9235e285e8f5a5ac8c92ecedb5
youngniiiiii <hanuje_@naver.com>
2023-08-21 21:54:14 +0900

    hook 추가

--- practice1/src/pages/Products.jsx
+++ practice1/src/pages/Products.jsx
@@ -1,8 +1,4 @@
-const enddpoint = `${import.meta.env.VITE_PB_API}/collections/products/records`
-
 function Products() {
-  useFetchData(endpoint)
-
   return (
     <section className={`h-screen  p-5`}>
       <h2>Products</h2>
